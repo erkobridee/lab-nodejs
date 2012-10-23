ea32b6be59b09f4aabe690cc322c25ab8037816f
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-10-23 21:20:09 -0200

    basic project structure generated with express.js 2.5.11

---
+++ web/expressjs/hello-node/README.md
@@ -0,0 +1,5 @@
+# About
+
+Basic express project initial structure
+
+* [express.js](http://expressjs.com/) v 2.5.11
---
+++ web/expressjs/hello-node/routes/index.js
@@ -0,0 +1,23 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res)
+{
+	res.render(
+		'index', 
+		{ title: 'Express' }
+	)
+};
+
+exports.about = function(req, res)
+{
+	res.render(
+  		'about', 
+		{ 
+			title: 'About',
+			content: 'Hello !' 
+		}
+	)
+};
