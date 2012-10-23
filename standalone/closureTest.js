7bb17ffe49366a14daafae4e309fd243c3038694
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-10-23 21:14:19 -0200

    simple closure test

---
+++ standalone/closureTest.js
@@ -0,0 +1,37 @@
+(function () {
+
+  alert = function(msg) {
+    console.log(msg);
+  }
+
+  A = (function() {
+    
+    function A() {}
+
+    A.prototype.att1 = 'attribute 1';
+
+    A.prototype.say = function(message) {
+      msg = message || 'class A say something';
+      alert(msg);
+    }
+
+    A.prototype.say2 = function(message) {
+      msg = message || 'class A say2 something';
+      alert(msg);
+    }
+
+    return A;
+
+  })();
+
+  
+  var aInstance = new A();
+  
+  alert(aInstance.att1);
+  aInstance.att1 = 'new value attribute 1';
+  alert(aInstance.att1);
+
+  aInstance.say();
+  aInstance.say2();
+
+}).call(this);
