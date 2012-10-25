12301a70f23da534f12c5966f2393f47f39706c8
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-10-25 10:19:50 -0200

    best way to associate some JS "class" to EventEmitter

---
+++ standalone/eventsTest.js
@@ -0,0 +1,64 @@
+// nodeEventsTest.js
+
+/*
+  references: 
+
+  - Emitting Custom Events in Node.js
+  http://venodesigns.net/2011/04/16/emitting-custom-events-in-node-js/
+
+  - Node.js Events Docs
+  http://nodejs.org/api/events.html
+
+*/
+//-----------------------------------------------------------------------
+
+//var events = require('events');  // 1
+//var EventEmitter = require('events').EventEmitter;  // 2
+
+var Test = (function() {
+
+  var Test = function () {};
+
+  //Test.prototype = new events.EventEmitter();  // 1
+  //Test.prototype.__proto__ = EventEmitter.prototype;  // 2
+  Test.prototype.__proto__ = require('events').EventEmitter.prototype;  // 3
+
+  Test.prototype.dispatch = function(eventName, data) {
+    var self = this;
+    self.emit(eventName, data);
+  };
+
+  return Test;
+})();
+
+//-----------------------------------------------------------------------
+
+function helloEventHandler(data) {
+  console.log('this is helloEventHandler');
+  if(data) console.log(data);
+}
+
+function customEventHandler() {
+  console.log('this is customEventHandler');
+}
+
+//-----------------------------------------------------------------------
+
+var test = new Test();
+
+// register events listeners
+test.on('helloEvent', helloEventHandler);
+test.on('customEvent', customEventHandler);
+
+// dispatchers
+test.dispatch('helloEvent', {hello: 'hello world'});
+test.dispatch('helloEvent');
+test.dispatch('customEvent');
+
+// remove events listeners
+test.removeListener('helloEvent', helloEventHandler);
+test.removeListener('customEvent', customEventHandler);
+
+//-----------------------------------------------------------------------
+
+console.log('Finished');
