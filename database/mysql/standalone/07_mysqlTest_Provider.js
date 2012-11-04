e0664ef38be9e79a3aa6b34350ba2ac8f9575f08
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-11-03 22:54:40 -0200

    new execution flow, for each operation use separated connection

--- database/mysql/standalone/07_mysqlTest_Provider.js
+++ database/mysql/standalone/07_mysqlTest_Provider.js
@@ -2,115 +2,36 @@
 
 //-----------------------------------------------------------------------------
 
-var Connection = (function() {
-
-  var EventEmitter = require('events').EventEmitter
-    , mysql = require('mysql')
-  ;
-
-  function handleConnectionLost(connection) {
-    connection.on('error', function(err) {
-      if (!err.fatal) {
-        return;
-      }
-
-      if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-        throw err;
-      }
-
-      console.log('Re-connecting lost connection: ' + err.stack);
-
-      connection = mysql.createConnection(connection.config);
-      handleDisconnect(connection);
-      connection.connect();
-    });
-  };
-
-  function DBConnection(options) {
-    if(!options) {
-      options = {
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'nodetest'
-      };
-    }
-
-    this.isConnected = false;
-    this.client = mysql.createConnection(options);
-
-    handleConnectionLost(this.client);
-  };
-
-  DBConnection.prototype.__proto__ = EventEmitter.prototype;
-
-  DBConnection.prototype.connect = function(callback) {
-    var self = this;
-    if(callback) {
-      this.client.connect(function(err) {
-        if(!err) self.isConnected = true;
-        callback(err);
-      });
-    } else {
-      this.client.connect(function(err) {
-        if(err) {
-          return self.emit('error', err);
-        } else {
-          self.isConnected = true;
-          return self.emit('connected');
-        }
-      });
-    }
-  };
-
-  DBConnection.prototype.disconnect = function(callback) {
-    var self = this;
-    if(callback) {
-      self.isConected = false;
-      this.client.end(callback);
-    } else {
-      this.client.end(function(err) {
-        if(err) { 
-          return self.emit('error', err); 
-        } else { 
-          self.isConected = false;
-          return self.emit('disconnected'); 
-        }
-      });
-    }
-  };
-
-  // class definition
-  return DBConnection;
-})();
-
-//-----------------------------------------------------------------------------
-
 var Provider = (function(){
 
   var EventEmitter = require('events').EventEmitter
+    , mysql = require('mysql')
     , async = require('async')
   ;
 
   //--- private
-  function dbOperation(connection, callback, sql, params) {
-    var err, results;
 
-    console.log('dbOperation');
+  // each operation use one isolated connection
+  function dbOperation(provider, callback, sql, params) {
+    var client, isConnected = false
+      , err, results;
+
+   if(provider.debug) console.log('dbOperation');
 
     // sync execution
     async.series({
       connect     : function(next) {
-        console.log('connect');
+        if(provider.debug) console.log('connect');
+        
+        client = mysql.createConnection(provider.dbconfig);
 
-        connection.connect(function(local_err) {
+        client.connect(function(local_err) {
           if(local_err) err = local_err;
+          else isConnected = true;
           next();
         });
       },
       execute     : function(next) {
-        console.log('execute');
-
         if(err) next();
 
         function queryCallback(local_err, rows) {
@@ -119,24 +40,24 @@ var Provider = (function(){
           next();
         };
 
-        console.log('do query');
-        console.log(sql);
+        if(provider.debug) console.log('execute query');
+        if(provider.debug) console.log(sql);
+
         if(params) {
-          console.log(params);
-          connection.client.query(sql, params, queryCallback);
+          if(provider.debug) console.log(params);
+          client.query(sql, params, queryCallback);
         } else {
-          console.log('no params');
-          connection.client.query(sql, queryCallback);
+          if(provider.debug) console.log('no params');
+          client.query(sql, queryCallback);
         }
       },
-      disconnect  : function(next) {   
-        
-        console.log('is connected? ' + connection.isConnected);
-        console.log('disconnect');
-
-        if(connection.isConnected) {
-          connection.disconnect(function(local_err) {
+      disconnect  : function(next) {  
+        if(provider.debug) console.log('is connected? ' + isConnected);
+        if(isConnected) {
+          if(provider.debug) console.log('disconnect');
+          client.end(function(local_err) {
             if(local_err) err = local_err;
+            else client.destroy();
             next();
           });
         } else {
@@ -144,15 +65,25 @@ var Provider = (function(){
         }
       },
       end         : function(next) { 
-        console.log('callback');
+        if(provider.debug) console.log('callback');
         callback(err, results); 
       }
     });
   }
 
   //--- constructor
-  function Provider(connection, table) {
-    this.connection = connection;
+  function Provider(table, dbconfig, debug) {
+    if(!dbconfig) {
+      dbconfig = {
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'nodetest'
+      };
+    }
+    //this.debug = debug ? true : false;
+    this.debug = false;
+    this.dbconfig = dbconfig;
     this.table = table;
   };
 
@@ -162,33 +93,38 @@ var Provider = (function(){
   Provider.prototype.findAll = function(callback) {
     var sql = 'SELECT * FROM ' + this.table;
 
-    return dbOperation(this.connection, callback, sql);
+    return dbOperation(this, callback, sql);
   };
 
   Provider.prototype.findById = function(id, callback) {
     var sql = 'SELECT * FROM ' + this.table + ' WHERE id = ?';
 
-    return dbOperation(this.connection, callback, sql, id);
+    function localCallback(err, rows) {
+      if(err) return callback(err);
+      else return callback(null, rows[0]);
+    };
+
+    return dbOperation(this, localCallback, sql, id);
   };
 
   Provider.prototype.insert = function(item, callback) {
     var sql = 'INSERT INTO ' + this.table + ' SET ?';
 
-    return dbOperation(this.connection, callback, sql, item);
+    return dbOperation(this, callback, sql, item);
   };
 
   Provider.prototype.update = function(id, item, callback) {
-    var sql = 'DELETE FROM ' + this.table + ' WHERE id = ?'
+    var sql = 'UPDATE ' + this.table + ' SET ? WHERE id = ?'
       , params = [item, id];
     ;
 
-    return dbOperation(this.connection, callback, sql, params);
+    return dbOperation(this, callback, sql, params);
   };
 
-  Provider.prototype.delete = function(id, callback) {
+  Provider.prototype.remove = function(id, callback) {
     var sql = 'DELETE FROM ' + this.table + ' WHERE id = ?';
 
-    return dbOperation(this.connection, callback, sql, id);
+    return dbOperation(this, callback, sql, id);
   };
 
   Provider.prototype.query = function(sql, values, callback) {
@@ -197,7 +133,7 @@ var Provider = (function(){
       values = null;
     }
 
-    return dbOperation(this.connection, callback, sql, values);
+    return dbOperation(this, callback, sql, values);
   };
   
   // class definition
@@ -205,16 +141,102 @@ var Provider = (function(){
 })();
 
 //-----------------------------------------------------------------------------
+// tests
 
-var conn = new Connection()
-  , products = new Provider(conn, 'products');
+var products = new Provider('products')
+  , async = require('async')
+  , lastId, product
 ;
 
-console.log('findAll products');
-products.findAll(function(err, results) {
-  console.log('result');
-  if(err) return console.log(err);
-  else return console.log(results);
-});
+function listProducts(doNext) {
+  console.log('findAll products');
+  products.findAll(function(err, results) {
+    console.log('result');
+    if(err)  return console.log(err);
 
-// TODO: test the other functions
+    console.log(results);
+    return doNext()
+  });
+};
+
+// sequence serie
+// start / insert / list / findById / update / list / delete / list / custom query / end
+async.series({
+  start: function(next) {
+    console.log('-=< START >=-');
+
+    next();
+  },
+  insert: function(next) {
+    console.log('insert product');
+
+    product = {name: 'product by provider', value: 1};
+
+    products.insert(product, function(err, result) {
+      console.log('result');
+      if(err) return console.log(err); 
+
+      lastId = result.insertId;
+      console.log('id: ' + lastId);
+      return next();
+    });    
+  },
+  list1: function(next) { listProducts(next); },
+  find:  function(next) {
+    console.log('findById');
+    products.findById(lastId, function(err, result) {
+      console.log('result');
+      if(err) return console.log(err); 
+
+      product = result;
+      console.log(result);
+      return next();
+    });
+  },
+  update: function(next) {
+    console.log('update');
+
+    var productToUpdate = {
+      name: product.name + ' UPDATED',
+      value: product.value * parseInt(Math.random() * 10),
+      dt: new Date()
+    };
+
+    products.update(lastId, productToUpdate, function(err, result) {
+      console.log('result');
+      if(err) return console.log(err);
+
+      console.log(result);
+      return next();
+    });
+  },
+  list2: function(next) { listProducts(next); },
+  remove: function(next) {
+    console.log('delete');
+
+    products.remove(lastId, function(err, result) {
+      console.log('result');
+      if(err) return console.log(err);
+
+      console.log(result);
+      return next();
+    });
+  },
+  list3: function(next) { listProducts(next); },
+  query: function(next) {
+    console.log('custom query');
+
+    var sql = 'SELECT NOW() AS now';
+
+    products.query(sql, function(err, result) {
+      console.log('result');
+      if(err) return console.log(err);
+
+      console.log(result);
+      return next();
+    });
+  },
+  end: function(next) {
+    console.log('-=< END >=-');    
+  }
+});
