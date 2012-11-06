bbdbb98487eadeb6180d8a8f5bee0f3e092a67e2
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-11-06 21:59:09 -0200

    tests with node.js global object

---
+++ standalone/globalTest.js
@@ -0,0 +1,29 @@
+
+// look at mainModule inside global object
+module.exports = function test1() {
+  return 'test';
+};
+
+// not finded inside global object
+exports = function test2() {
+  return /[a-z]/i
+};
+
+// added at end of global object
+global.App = {
+  att1: 'test att1',
+  att2: 'test att2'
+};
+
+console.log('\n---GLOBAL: START---------------------------------------');
+console.log(global);
+console.log('\n---GLOBAL: END-----------------------------------------');
+
+console.log('\n\tsame object? (GLOBAL === ROOT) ' + (global === root));
+
+console.log('\n\t__dirname = shortcut to global.env.PWD');
+
+console.log('\n\t__dirname : where i\'m?\n');
+console.log( __dirname );
+
+console.log('\n---END-------------------------------------------------\n');
