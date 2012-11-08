5f48bf9fde222e6ca557d946af9d595bd1b0712f
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-11-08 02:18:50 -0200

    string reverse and base64 tests

---
+++ standalone/stringReverseAndBase64Test.js
@@ -0,0 +1,74 @@
+
+function reverseStr(value) {
+  splitext = value.split("");
+  revertext = splitext.reverse();
+  reversed = revertext.join("");
+  return reversed;  
+};
+
+String.prototype.reverse = function() {
+  splitext = this.split("");
+  revertext = splitext.reverse();
+  reversed = revertext.join("");
+  return reversed;
+};
+
+//-------------------------------------------
+
+var x = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec est ac lectus gravida egestas vitae id lorem.';
+
+console.log('\n');
+console.log('x : ' + x);
+console.log('x.reverse() : ' + x.reverse());
+console.log('\n');
+
+x = reverseStr(x);
+
+console.log(x);
+
+x = reverseStr(x);
+
+console.log(x);
+
+console.log('\n');
+
+//-------------------------------------------
+
+var Base64 = require('./lib/base64'); 
+
+/*
+console.log(Base64);
+console.log('\n');
+*/
+
+console.time('encode');
+x = reverseStr(x);
+console.log('0: ' + x);
+x = Base64.encode(x);
+console.log('1: ' + x);
+x = reverseStr(x);
+console.log('2: ' + x);
+x = Base64.encode(x);
+console.log('3: ' + x);
+console.timeEnd('encode');
+
+console.log('\n');
+
+console.time('decode');
+x = Base64.decode(x);
+console.log('3: ' + x);
+x = reverseStr(x);
+console.log('2: ' + x);
+x = Base64.decode(x);
+console.log('1: ' + x);
+x = reverseStr(x);
+console.log('0: ' + x);
+console.timeEnd('decode');
+
+console.log('\n');
+
+console.log(x);
+
+console.log('\n');
+
+//-------------------------------------------
