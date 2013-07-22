62d2ad9e7ba1df312ef8b0da37013b7461c59805
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-07-22 15:57:31 -0300

    review ClousureTest

--- javascript/standalone/closureTest.js
+++ javascript/standalone/closureTest.js
@@ -1,37 +1,117 @@
 (function () {
 
-  var alert = function(msg) {
+  //------------------------------------------
+
+  var log = function(msg) {
     console.log(msg);
   }
 
+  //------------------------------------------
+
+  // Class Scope
   var A = (function() {
-    
-    function A() {}
 
-    A.prototype.att1 = 'attribute 1';
+    // Constructor
+    function ClassDef() {};
 
-    A.prototype.say = function(message) {
+    // static ------------------------------------------
+
+      // vars
+    ClassDef.staticAtt = 'static attribute value';
+
+      // functions
+    ClassDef.staticHelloMessage = function() {
+      return 'static hello message';
+    };
+
+
+    // private ------------------------------------------
+
+      // vars
+    var privateAtt = 'private attribute value';
+
+      // function
+    // var privateHelloMessage = function() {  // both ways to declare one function
+    function privateHelloMessage() {
+      return 'private hello message';
+    };
+
+
+    // public ------------------------------------------
+
+      // vars
+    ClassDef.prototype.publicAtt = 'public attribute value';
+
+      // functions
+    ClassDef.prototype.publicHelloMessage = function() {
+      return 'public hello message';
+    };
+
+        //---
+
+    ClassDef.prototype.setPrivateAtt = function(value) {
+      privateAtt = value;
+    };
+
+    ClassDef.prototype.getPrivateAtt = function() {
+      return privateAtt;
+    };
+
+    ClassDef.prototype.getPrivateHelloMessage = function() {
+      return privateHelloMessage();
+    };
+
+        //---
+
+    ClassDef.prototype.say = function(message) {
+      // if message not defined, set default value
       msg = message || 'class A say something';
-      alert(msg);
-    }
+      log(msg); // call parent function
+    };
 
-    A.prototype.say2 = function(message) {
+    ClassDef.prototype.say2 = function(message) {
+      // if message not defined, set default value
       msg = message || 'class A say2 something';
-      alert(msg);
-    }
+      log(msg); // call parent function
+    };
 
-    return A;
+    //---
+
+    // return class definition
+    return ClassDef;
 
   })();
 
+  //------------------------------------------
+
+  log(A); // class defined
+
+  log('------');
+
+  // statics
+  log(A.staticAtt);
+  log(A.staticHelloMessage());
   
+  log('------');  
+
   var aInstance = new A();
   
-  alert(aInstance.att1);
-  aInstance.att1 = 'new value attribute 1';
-  alert(aInstance.att1);
+  log(aInstance.publicAtt);
+  aInstance.publicAtt = 'new value to public attribute';
+  log(aInstance.publicAtt);
+
+  log('------'); 
 
   aInstance.say();
   aInstance.say2();
 
-}).call(this);
+  log('------'); 
+
+  log(aInstance.getPrivateHelloMessage());
+  log(aInstance.getPrivateAtt());
+  aInstance.setPrivateAtt('new private attribute');
+  log(aInstance.getPrivateAtt());
+
+})();
+
+console.log(A); // ReferenceError: A is not defined
