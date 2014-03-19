25169cc8753667e641d5b1f561df86570b548b13
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-03-19 16:15:01 -0300

    ast : js-ast-js : require-config-rewrite.js working :D

--- javascript/ast/js-ast-js/require-config-rewrite.js
+++ javascript/ast/js-ast-js/require-config-rewrite.js
@@ -10,31 +10,114 @@ var Q = require('q')
 //-------------------------------
 
 var inputSrc = '../src/require.config.js',
-    outputSrc = '../output/require.config.js';
-
-var astJson = '../output/require.config.ast.json';
+    outputSrc = '../output/require.build.config.js';
 
 //-------------------------------
 
 // 1 - read source code
-function readSourceCode(file) {
+function readSourceCode() {
+  
+  fs_readfile(inputSrc, 'utf-8')
+    .then(generateAST)
+    .then(editAST)
+    .then(writeNewSourceCode);
 
 }
 
 // 2 - generate AST
 function generateAST(codeStr) {
+  
   return esprima.parse(codeStr);
+
 }
 
 // 3 - edit AST
 function editAST(ast) {
+  
+  //---
+
+  function checkTargetNode(node) {
+    var flag = false;
+
+    if(
+      node.type === 'Property' &&
+      node.key.type === 'Identifier' &&
+      ( 
+        node.key.name === 'paths' || 
+        node.key.name === 'deps'
+      )
+    ) {
+      flag = true;
+    }
+
+    return flag;
+  };
+
+  function newNodeValue() {
+    return {
+      type: 'Literal',
+      value: 'empty:'
+    };
+  }
+
+  //---
+
+  // get require config object : require( {} )
+  var configAST = ast.body[0].expression.arguments[0];
+
+  var oldProperties = configAST.properties,
+      newProperties = [],
+      paths;
+
+  // get only what is needed
+  oldProperties.forEach(function(node) {
+    
+    if(checkTargetNode(node)) {
+      newProperties.push(node);
+      if(node.key.name === 'paths') paths = node;
+    }
+    
+  });
+
+  // define new value
+  paths.value.properties.forEach(function(node) {
+    node.value = newNodeValue();
+  });
+
+  // update
+  configAST.properties = newProperties;
+
+  //---
+
+  // code style options
+  var escodegenOptions = {
+    format: {
+      indent: {
+        style: '  '
+      }
+    }
+  };
+
+  // generate new source code
+  return escodegen.generate(ast, escodegenOptions);
 
 }
 
 // 4 - write new source code
 function writeNewSourceCode(codeStr) {
+  // https://github.com/jprichardson/node-fs-extra
+
+  console.log(codeStr);
+
+  fse.outputFile(outputSrc, codeStr, function(err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
+
+  console.log('\n\nend\n');
 
 }
 
 //-------------------------------
 
+readSourceCode();
