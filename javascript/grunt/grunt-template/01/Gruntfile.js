8a73246ee7d351ac441dcaa55afc9f2bdf2688fa
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-06-04 21:33:32 -0300

    grunt-template / 01 - Gruntfile.js rewrited

---
+++ javascript/grunt/grunt-template/01/Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  require('time-grunt')(grunt);
+
+  // Initialize config
+  grunt.initConfig({
+    pkg: require('./package.json'),
+  });
+
+  // tasks config per file
+  grunt.loadTasks('grunt_config');
+
+  //--- grunt tasks
+
+  grunt.registerTask('default', ['jshint', 'clean']);
+
+  grunt.registerTask('dev', ['clean','jshint', 'template:dev']);
+
+  grunt.registerTask('prod', ['clean','jshint', 'template:prod']);
+
+};
