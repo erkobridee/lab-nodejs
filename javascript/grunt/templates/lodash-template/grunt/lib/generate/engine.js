cbdbc42e0e523850fd25b3de4885281d976f496c
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-07-14 16:55:19 -0300

    template generate engine reviewed
    now with two flows, one for single template file, and another for
    templates inside some directory + Custom error for template processment

--- javascript/grunt/templates/lodash-template/grunt/lib/generate/engine.js
+++ javascript/grunt/templates/lodash-template/grunt/lib/generate/engine.js
@@ -13,6 +13,7 @@ var _     = require('lodash-node'),
 module.exports = function generate(options) {
 
   //---------------------------------------------
+  // check options
 
   /*
   var options = {
@@ -33,78 +34,173 @@ module.exports = function generate(options) {
   var _updateFileName = options.updateFileName || function( name ) { return name; };
 
   //---------------------------------------------
-  // internal functions
 
-  function filterOnlyFiles(path, stat) {
-    // ignore dot files like .DS_Store
-    var filename = path.split('/').pop();
-    return stat.isFile() && !filename.match(/^\./);
+  function TemplateProcessError(message, error) {
+    this.constructor.prototype.__proto__ = Error.prototype;
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+
+    this.toString = function() {
+      var out = this.name + ': \n  ' + this.message;
+      if(error) out += '\nTemplate Error :\n  ' + error.stack;
+      return out;
+    }
   }
 
   //---------------------------------------------
-  // internal variables
 
-  var metadata = {};
-  var templateCache = {};
+  function processDirectory() {
+
+    var metadata = {};
+    var templateCache = {};
+
+    function filterOnlyFiles(path, stat) {
+      // ignore dot files like .DS_Store
+      var filename = path.split('/').pop();
+      return stat.isFile() && !filename.match(/^\./);
+    }
+
+    return fs.listTree(source, filterOnlyFiles) // list files
+      .then(function(files) {
+
+        var templateName, output;
+
+        return Q.all(files.sort().map(function(filePath) {
+
+          return fs.read(filePath).then(function(content) { // read template file
+
+            templateName = filePath.replace(source + '/', '');
+
+            output = {
+              path: filePath,
+              name: path.basename(filePath),
+              templateName: templateName,
+              directory: path.dirname( filePath ).replace(source, ''),
+              content: content
+            };
+
+            metadata[templateName] = output;
+
+            try{
+              templateCache[templateName] = _.template(content);
+            } catch(err) {
+              throw new TemplateProcessError(filePath, err);
+            }
+
+            return output;
+
+          });
+
+        }));
+      })
+      .then(function(files) { // compile and write files
+
+        Q.all(files.map(function(file) {
+
+          var fileUrl, content;
+
+          // define file destination
+          fileUrl = path.join(
+            destination, // output directory
+            values.name, // processed templates directory output
+            file.directory, // exist some sub directory ?
+            _updateFileName( file.name ) // output file name
+          );
+
+
+          // process template
+          content = templateCache[file.templateName](values);
+
+
+          // create directories tree
+          return fs.makeTree(path.dirname(fileUrl)).then(function() {
+            return fs.write(fileUrl, content); // write file to disk
+          });
+
+        }));
+
+
+      });
+
+  }
+
+  //---------------------------------------------
+
+  function processFile() {
+
+    return fs.read(source).then(function(content) { // read template file
+
+      var tplCache = null;
+      try{
+        tplCache = _.template(content);
+      } catch(err) {
+        throw new TemplateProcessError(source, err);
+      }
+
+      return {
+        name: path.basename(filePath),
+        templateCache: tplCache
+      };
+
+    })
+    .then(function(file) {
+
+      var fileUrl, content;
+
+      // define file destination
+      fileUrl = path.join(
+        destination, // output directory
+        _updateFileName( file.name ) // output file name
+      );
+
+
+      // process template
+      content = file.templateCache(values);
+
+
+      // create directories tree
+      return fs.makeTree(path.dirname(fileUrl)).then(function() {
+        return fs.write(fileUrl, content); // write file to disk
+      });
+
+    });
+
+  }
 
   //---------------------------------------------
   // templates processment flow
 
-  return fs.listTree(source, filterOnlyFiles) // list files
-    .then(function(files) {
+  // check if options.source is file or directory
+  return fs.exists( source ).then(function( flag ) {
 
-      var templateName, output;
+    if( flag ) {
 
-      return Q.all(files.sort().map(function(filePath) {
+      return fs.stat( source ).then(function( stat ) {
 
-        return fs.read(filePath).then(function(content) { // read template file
+        if( stat.isDirectory() ) {
 
-          templateName = filePath.replace(source + '/', '');
+          return processDirectory(); // directory templates processment flow
 
-          output = {
-            path: filePath,
-            name: path.basename(filePath),
-            templateName: templateName,
-            directory: path.dirname( filePath ).replace(source, ''),
-            content: content
-          };
+        } else if( stat.isFile() ) {
 
-          metadata[templateName] = output;
-          templateCache[templateName] = _.template(content);
+          return processFile(); // file template processment flow
 
-          return output;
+        } else {
 
-        });
+          throw new Error( 'Invalid : ' + source );
 
-      }));
-    })
-    .then(function(files) { // compile and write files
+        }
 
-      Q.all(files.map(function(file) {
+      });
 
-        var fileUrl, content;
+    } else {
 
-        // define file destination
-        fileUrl = path.join(
-          destination, // output directory
-          values.name, // processed templates directory output
-          file.directory, // exist some sub directory ?
-          _updateFileName( file.name ) // output file name
-        );
+      throw new Error( 'Not found : ' + source );
 
+    }
 
-        // process template
-        content = templateCache[file.templateName](values);
+  });
 
-
-        // create directories tree
-        return fs.makeTree(path.dirname(fileUrl)).then(function() {
-          return fs.write(fileUrl, content); // write file to disk
-        });
-
-      }));
-
-
-    });;
 
 };
