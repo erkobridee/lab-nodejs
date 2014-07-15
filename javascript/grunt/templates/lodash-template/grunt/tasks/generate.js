ee082e1dc0abad01e68aff3477566162be82ded1
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-07-15 11:45:33 -0300

    lodash-template : grunt task generate
    testing lib/generate/engine.js

--- javascript/grunt/templates/lodash-template/grunt/tasks/generate.js
+++ javascript/grunt/templates/lodash-template/grunt/tasks/generate.js
@@ -1,10 +1,21 @@
 module.exports = function(grunt) {
   'use strict';
 
+  var _       = require('lodash-node');
+  var _s      = require( 'underscore.string' );
+  var path    = require( 'path' );
+  var engine  = require( '../lib/generate/engine' );
+
+
+  console.log(engine);
+
   grunt.registerTask(
     'generate',
     'generate files based on templates',
   function(template, value, outputDir) {
+
+    // Async Task
+    var done = this.async();
 
     var options = this.options({}),
         tplsDir = options.templatesDir || 'templates',
@@ -27,9 +38,75 @@ module.exports = function(grunt) {
 
     }
 
+    /*
     grunt.log.write( JSON.stringify( this, null, 2 ) );
     grunt.log.write( '\n\n' );
     grunt.log.write( JSON.stringify( options, null, 2 ) );
+    */
+
+    //------
+
+    // templates/hello_world.jst
+    /*
+    var engineOptions = {
+      source: path.join(tplsDir, 'hello_world.jst'),
+      destination: defaultOutputDir,
+      values: {
+        value: 'world',
+      },
+      updateFileName: function(name) {
+        return 'hello_world.txt';
+      }
+    };
+    */
+
+
+    // templates/tplsDir
+
+    var values = {
+      name: 'useCase',
+      users: ['user 1', 'user 2', 'user 3', 'user ...']
+    };
+
+    _.assign(values, {
+
+      helpers: {
+        capitalize: function(value) {
+          return _s.capitalize(value);
+        },
+
+        updateFileName: function(sName) {
+          var update = false;
+
+          if( update ) {
+            sName = values.name + _s.capitalize( sName );
+          }
+
+          return sName;
+        }
+      }
+
+    });
+
+
+    var engineOptions = {
+      source: path.join(tplsDir, '/tplsDir'),
+      destination: defaultOutputDir,
+      debug: true,
+      values: values,
+      updateFileName: values.helpers.updateFileName
+    };
+
+
+
+    engine( engineOptions )
+    .then(function() {
+      done();
+    })
+    .catch(function(e) {
+      grunt.fail.fatal(e.stack);
+      done(e);
+    });
 
   });
 
