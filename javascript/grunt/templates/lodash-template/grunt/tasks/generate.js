c71d3be3274381c8d3d9e9a452e2dce595b2520b
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-07-23 15:20:48 -0300

    lodash-template : grunt generate task reviewed

--- javascript/grunt/templates/lodash-template/grunt/tasks/generate.js
+++ javascript/grunt/templates/lodash-template/grunt/tasks/generate.js
@@ -12,14 +12,11 @@ module.exports = function(grunt) {
     'generate files based on templates',
   function(template, value, outputDir) {
 
-    // Async Task
-    var done = this.async();
-
     var options = this.options({}),
         tplsDir = options.templatesDir || 'templates',
         defaultOutputDir = options.outputDir || 'dist';
 
-
+    /*
     grunt.log.write( 'args length: ' +  this.args.length + '\n\n' );
 
     grunt.log.write( 'template:value:outputDir \n\n' );
@@ -35,6 +32,7 @@ module.exports = function(grunt) {
       grunt.log.write( 'ask questions\n\n' );
 
     }
+    */
 
     /*
     grunt.log.write( JSON.stringify( this, null, 2 ) );
@@ -44,66 +42,28 @@ module.exports = function(grunt) {
 
     //------
 
-    // templates/hello_world.jst
-    /*
-    var engineOptions = {
-      source: path.join(tplsDir, 'hello_world.jst'),
+    // Async Task
+    var done = this.async();
+
+    //grunt.log.write( '\n\n' );
+
+    questions({
+      source: tplsDir,
       destination: defaultOutputDir,
-      values: {
-        value: 'world',
-      },
-      updateFileName: function(name) {
-        return 'hello_world.txt';
-      }
-    };
-    */
+      restContext: 'apirest' // TODO: define task config
+    })
+    .then(function( optionsToEngine ) {
 
+      // TODO: define end string message on engine
+      return engine( optionsToEngine );
 
-    // templates/tplsDir
-
-    var values = {
-      name: 'useCase',
-      users: ['user 1', 'user 2', 'user 3', 'user ...']
-    };
-
-    _.assign(values, {
-
-      helpers: {
-        capitalize: function(value) {
-          return _s.capitalize(value);
-        },
-
-        updateFileName: function(sName) {
-          var update = false;
-
-          if( update ) {
-            sName = values.name + _s.capitalize( sName );
-          }
-
-          return sName;
-        }
-      }
-
-    });
-
-
-    var engineOptions = {
-      source: path.join(tplsDir, '/tplsDir'),
-      destination: defaultOutputDir,
-      debug: true,
-      values: values,
-      updateFileName: values.helpers.updateFileName
-    };
-
-
-
-    engine( engineOptions )
+    })
     .then(function() {
-      done();
+      return done( true );
     })
     .catch(function(e) {
       grunt.fail.fatal(e.stack);
-      done(e);
+      return done(e);
     });
 
   });
