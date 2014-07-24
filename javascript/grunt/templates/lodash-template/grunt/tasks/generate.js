d538f14f2a8f6d09f273106e168a73ef3c0f2c06
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-07-23 23:02:14 -0300

    lodash-templates : grunt task generate
    verify if backend restContext is defined

--- javascript/grunt/templates/lodash-template/grunt/tasks/generate.js
+++ javascript/grunt/templates/lodash-template/grunt/tasks/generate.js
@@ -14,47 +14,22 @@ module.exports = function(grunt) {
 
     var options = this.options({}),
         tplsDir = options.templatesDir || 'templates',
-        defaultOutputDir = options.outputDir || 'dist';
+        defaultOutputDir = options.outputDir || 'dist',
+        restContext = options.restContext || 'rest';
 
-    /*
-    grunt.log.write( 'args length: ' +  this.args.length + '\n\n' );
-
-    grunt.log.write( 'template:value:outputDir \n\n' );
-
-    if( this.args.length > 0 ) {
-
-      grunt.log.write( 'template  : ' +  this.args[0] + ' | ' + template + '\n' );
-      grunt.log.write( 'value     : ' +  this.args[1] + ' | ' + value + '\n' );
-      grunt.log.write( 'outputDir : ' +  this.args[2] + ' | ' + outputDir + '\n\n' );
-
-    } else {
-
-      grunt.log.write( 'ask questions\n\n' );
-
-    }
-    */
-
-    /*
-    grunt.log.write( JSON.stringify( this, null, 2 ) );
-    grunt.log.write( '\n\n' );
-    grunt.log.write( JSON.stringify( options, null, 2 ) );
-    */
-
-    //------
+    //---
 
     // Async Task
     var done = this.async();
 
-    //grunt.log.write( '\n\n' );
-
     questions({
       source: tplsDir,
       destination: defaultOutputDir,
-      restContext: 'apirest' // TODO: define task config
+      restContext: restContext
     })
     .then(function( optionsToEngine ) {
 
-      // TODO: define end string message on engine
+      // TODO: define and return some string message on engine when finished?
       return engine( optionsToEngine );
 
     })
