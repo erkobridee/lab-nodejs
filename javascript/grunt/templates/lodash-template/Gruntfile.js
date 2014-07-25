d5a0d70a800e5700190a4e842dabc0148d1aa4d9
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-07-25 14:44:32 -0300

    lodash-templates : Gruntfile.js code reviewed

--- javascript/grunt/templates/lodash-template/Gruntfile.js
+++ javascript/grunt/templates/lodash-template/Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['clean', 'jshint']);
 
-  // TODO: define 'generate task
+  // grunt generate task defined
+  // run command 'grunt generate'
 
 };
