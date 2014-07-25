fb24734d28432fbc48bc84cd0d2f2d50c0f1bf37
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-07-24 21:24:26 -0300

    standalone / regexp string escape test

---
+++ javascript/standalone/regexpStringEscapeTest.js
@@ -0,0 +1,22 @@
+/*
+
+[StackOverflow] Can you create JavaScript regexes on the fly using string variables?
+https://stackoverflow.com/questions/4371565/can-you-create-javascript-regexes-on-the-fly-using-string-variables
+
+
+JavaScript: How to generate a regular expression from a string
+http://makandracards.com/makandra/15879-javascript-how-to-generate-a-regular-expression-from-a-string
+
+*/
+
+RegExp.escape = function(string) {
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+};
+
+function buildRegexp(string) {
+  return new RegExp(RegExp.escape(string));
+}
+
+console.log( buildRegexp( 'makandra.com' ) );
+
+console.log( buildRegexp( 'apirest/resurce' ) );
