52e1d5b6d231627755d261a064a1fd6ae81bfe95
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-07 17:01:55 -0300

    Karma / 02 require.js / 02 - updated
    karma shared config defined

---
+++ javascript/test-runner/karma/02_requirejs/02/configs/karma.shared.conf.js
@@ -0,0 +1,33 @@
+module.exports = function() {
+
+  return {
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '../',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'requirejs'],
+
+
+    // https://karma-runner.github.io/0.12/config/files.html
+
+    // list of files / patterns to load in the browser
+    files: [
+      'tests/require.config.js',
+      {pattern: 'src/**/*.js', included: false, served: true},
+      {pattern: 'tests/**/*.js', included: false, served: true},
+      {pattern: 'bower_components/**/*.{js,map}', included: false, served: true, watched: false},
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      'src/require.config.js',
+      'bower_components/**/src/**/*'
+    ]
+
+  };
+
+};
