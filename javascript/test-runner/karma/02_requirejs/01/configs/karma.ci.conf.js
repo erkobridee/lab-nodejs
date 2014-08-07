37df175ea33e49b71a0da3296be532d4390e6797
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-07 14:39:25 -0300

    Karma / 02 require.js / 01 - karma confs updated
    update web server port
    - karma.ci.conf.js >> 9871
    - karma.coverage.conf.js >> 9872

--- javascript/test-runner/karma/02_requirejs/01/configs/karma.ci.conf.js
+++ javascript/test-runner/karma/02_requirejs/01/configs/karma.ci.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
 
 
     // web server port
-    port: 9876,
+    port: 9871,
 
 
     // enable / disable colors in the output (reporters and logs)
--- javascript/test-runner/karma/02_requirejs/01/configs/karma.coverage.conf.js
+++ javascript/test-runner/karma/02_requirejs/01/configs/karma.coverage.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
 
 
     // web server port
-    port: 9876,
+    port: 9872,
 
 
     // enable / disable colors in the output (reporters and logs)
