d619afd1c6099f6e59537832c99f2f8a65bd90d8
Erko Bridee <erko.bridee@gmail.com>
2014-09-18 09:44:40 -0300

    update protractor config to non angular site

--- javascript/runner/test/protractor/01/non-angular/config/basic.js
+++ javascript/runner/test/protractor/01/non-angular/config/basic.js
@@ -2,6 +2,8 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
-    '../tests/e2e/*.spec.js'
+    //'../tests/e2e/*.spec.js'
+    '../tests/e2e/wikipedia-search.spec.js',
+    '../tests/e2e/correios-cep-search.spec.js'
   ]
 }
