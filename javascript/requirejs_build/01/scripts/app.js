9a3b9f6b5bc2e42923b7d8ec9d1facaa51ee9867
Erko Bridee <erko.bridee@gmail.com>
2014-12-05 13:59:11 -0200

    requirejs_build/01 call 2 new modules

--- javascript/requirejs_build/01/scripts/app.js
+++ javascript/requirejs_build/01/scripts/app.js
@@ -5,5 +5,12 @@ define(function(require) {
 
   console.log('running...');
 
+  var jrender = require('./jquery-render');
+
+  jrender.append();
+
+  var vrender = require('./vanilla-render');
+
+  vrender.append();
 
 });
