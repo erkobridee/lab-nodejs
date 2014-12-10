ff15cd574ec3d03b235b1f7c1942751647125763
Erko Bridee <erko.bridee@gmail.com>
2014-12-10 21:13:36 -0200

    grunt uglify config reviewed

--- javascript/runner/task/grunt/require-build-grunt/01/helpers/grunt/config/uglify.js
+++ javascript/runner/task/grunt/require-build-grunt/01/helpers/grunt/config/uglify.js
@@ -1,10 +1,14 @@
-module.exports = {
+module.exports = function(grunt) {
+
+grunt.config('uglify', {
 
   dist: {
     files: {
       '<%= project.paths.dist %>/require.config.js': ['<%= project.paths.dist %>/require.config.js'] ,
-     '<%= project.paths.dist %>/shared/fallback/ie.js': ['<%= project.paths.dist %>/shared/fallback/ie.js'] 
+      '<%= project.paths.dist %>/shared/fallback/ie.js': ['<%= project.paths.dist %>/shared/fallback/ie.js']
     }
   }
+
+});
 
 };
