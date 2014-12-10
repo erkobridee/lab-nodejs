ecaef747b77b40d69baaa6170b742ac5842eeb16
Erko Bridee <erko.bridee@gmail.com>
2014-12-10 20:55:23 -0200

    grunt clean config reviewed

--- javascript/runner/task/grunt/require-build-grunt/01/helpers/grunt/config/clean.js
+++ javascript/runner/task/grunt/require-build-grunt/01/helpers/grunt/config/clean.js
@@ -1,7 +1,11 @@
-module.exports = {
+module.exports = function(grunt) {
+
+grunt.config('clean', {
 
   build: ['<%= project.paths.build %>/'],
 
   dist: ['<%= project.paths.dist %>/']
-  
+
+});
+
 };
