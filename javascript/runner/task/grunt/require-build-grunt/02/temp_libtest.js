292b6416e5cbe2f32300e822c41da097fbaa8a5e
Erko Bridee <erko.bridee@gmail.com>
2014-12-15 19:36:30 -0200

    require-build-grunt/02 updated
    define mainModule and excludeModule parameters for require build
    modules array lib

--- javascript/runner/task/grunt/require-build-grunt/02/temp_libtest.js
+++ javascript/runner/task/grunt/require-build-grunt/02/temp_libtest.js
@@ -3,41 +3,16 @@ var findRequireModules = require('./helpers/lib/find-require-modules');
 var source = './src/app/modules';
 var fileMatch = /package\.js$/;
 var removeBase = 'src/';
+var mainModule = '_ng.app_';
+var excludeModule = '_require.config_';
 // var removeBase = 1;
 
-findRequireModules(source, fileMatch, removeBase)
+findRequireModules(
+  source, fileMatch, removeBase,
+  mainModule, excludeModule
+)
   .then(function modules(modulesArray) {
 
     console.log(modulesArray);
 
   });
-
-/* old
-findRequireModules(source, fileMatch, removeBase)
-  .then(function filesPath(files) {
-
-    var resultTxt = '\nrequire.js modules\n\n';
-
-    var modules = [{name: 'ng.app'}];
-
-    files.forEach(function(filename) {
-      resultTxt += filename + '\n';
-
-      modules.push(addModule(filename));
-    });
-
-    console.log( resultTxt );
-
-    console.log( modules );
-
-  });
-
-
-function addModule(filename) {
-  var name = filename.replace( '.js', '' );
-  return {
-    name: name,
-    exclude: ['require.config']
-  };
-}
-*/
