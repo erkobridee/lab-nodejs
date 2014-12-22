b5cf2ae0c8634c41dbadbf28d45f94b532f2909d
Erko Bridee <erko.bridee@gmail.com>
2014-12-22 11:54:32 -0200

    code review

--- javascript/web/http/simpleHttpServer.js
+++ javascript/web/http/simpleHttpServer.js
@@ -1,8 +1,9 @@
 var http = require("http");
 
 http.createServer(
-	function(request, response) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.end("Hello Web! \n");
-	}
-).listen(1337);
+  function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Web! \n");
+  }
+).listen(9000);
+// ).listen(1337);
