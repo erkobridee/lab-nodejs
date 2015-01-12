f6f54732f1186dd1d90a13179e2654c6d3198fff
Erko Bridee <erko.bridee@gmail.com>
2015-01-12 10:28:52 -0200

    gulp/uglify-gulp reviewed

--- javascript/runner/task/gulp/uglify-gulp/gulpfile.js
+++ javascript/runner/task/gulp/uglify-gulp/gulpfile.js
@@ -1,13 +1,31 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
+var path    = require('path');
+var del     = require('del');
+var gulp    = require('gulp');
+var rename  = require('gulp-rename');
+var uglify  = require('gulp-uglify');
+
+var config = {
+  src: 'src',
+  dist: 'dist'
+};
+
+gulp.task('clean:css', function(cb) {
+  del([
+    config.dist
+  ], cb);
+});
+
+gulp.task('clean', ['clean:css']);
 
 gulp.task('build', function() {
   gulp
-    .src('./src/hello-uglify.js')
+    .src(path.join(config.src, 'hello-uglify.js'))
     .pipe(uglify())
-    .pipe(rename('hello-uglify.min.js'))
-    .pipe(gulp.dest('./dist'))
+    // .pipe(rename('hello-uglify.min.js'))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest(config.dist))
 });
 
 gulp.task('default', ['build']);
