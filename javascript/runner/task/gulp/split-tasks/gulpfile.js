f0f8a64a91b94c947ac0631a40725538ff002e3a
Erko Bridee <erko.bridee@gmail.com>
2015-01-14 15:32:56 -0200

    gulp/split-tasks - gulpfile.js updated
    define tasks dir to load

--- javascript/runner/task/gulp/split-tasks/gulpfile.js
+++ javascript/runner/task/gulp/split-tasks/gulpfile.js
@@ -5,10 +5,13 @@
   https://gist.github.com/branneman/8048520
 */
 
+/*
 global.rootRequire = function(name) {
   return require(__dirname + '/' + name);
 };
+*/
 
 //---
 
-require('./tools/gulp');
+require('./tools/gulp')
+  .loadTasks('tools/gulp/tasks'); // load tasks config per file
