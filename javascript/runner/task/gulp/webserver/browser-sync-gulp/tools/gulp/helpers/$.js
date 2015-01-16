99ce0cba760f565b4a29ff357b8a1e823322d483
Erko Bridee <erko.bridee@gmail.com>
2015-01-16 09:59:54 -0200

    gulp / ... / browser-sync-gulp - updated
    webserver configs

--- javascript/runner/task/gulp/webserver/browser-sync-gulp/tools/gulp/helpers/$.js
+++ javascript/runner/task/gulp/webserver/browser-sync-gulp/tools/gulp/helpers/$.js
@@ -47,20 +47,29 @@ $.config = require('../../config');
 // @begin: define output dir
 
 (function() {
+
   var output = $.config.paths.dist || 'dist';
   if( $.is.cdn && $.is.release ) {
     output = $.path.join( output, $.pkg.name, $.pkg.version );
   }
   $.config.paths.outputDir = output;
+
 })();
 
 // @end: define output dir
 //---
 // @begin: check webserver configs
 
-$.config.webserver = $.config.webserver || {};
+(function() {
 
-$.config.webserver.port = parseInt(args.port, 10) || $.config.webserver.port || 3000;
+  $.config
+    .webserver = $.config.webserver || {};
+
+  $.config
+    .webserver
+    .port = parseInt(args.port, 10) || $.config.webserver.port || 3000;
+
+})();
 
 // @end: check webserver configs
 //---
