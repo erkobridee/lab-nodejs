94969d40d818c8c65cbc1a6811fca51b198d4050
Erko Bridee <erko.bridee@gmail.com>
2015-02-09 11:50:13 -0200

    livetpls xml process to markdown text

---
+++ javascript/xml/livetpls_xml_parse.js
@@ -0,0 +1,56 @@
+var fs = require('fs'),
+    path = require('path'),
+    Q = require('q'),
+    xml2js = require('xml2js');
+
+var parser = new xml2js.Parser();
+
+var fs_readfile = Q.denodeify( fs.readFile );
+var parseString = Q.denodeify( parser.parseString );
+
+var xmlPath = path.join(__dirname, 'src', 'livetpls.xml');
+
+//---
+
+fs_readfile( xmlPath )
+  .then(function fileContent( content ) {
+    return parseString( content );
+  })
+  .then(function processXML( xml ) {
+    return processLivetpls( xml, 'Require.js', 'JavaScript' );
+  })
+  .then(function result( markdown ) {
+    console.log( markdown );
+    console.log('');
+    console.log('done');
+  });
+
+//---
+
+function processLivetpls( xml, tplGroupName, lang ) {
+
+  var output = '';
+
+  var templateSet = xml.templateSet;
+
+  output += '## ' + tplGroupName + '\n';
+  output += '> templateSet group name: ' + templateSet.$.group + '\n';
+
+  for (var i = templateSet.template.length - 1; i >= 0; i--) {
+    output += processTemplate( templateSet.template[i], lang );
+  };
+
+  return output;
+
+}
+
+function processTemplate( template, lang ) {
+  var output = '';
+
+  output += '`' + template.$.name + '` - ' + template.$.description + '\n';
+  output += '```'+ lang;
+  output += template.$.value;
+  output += '```\n';
+
+  return output;
+}
