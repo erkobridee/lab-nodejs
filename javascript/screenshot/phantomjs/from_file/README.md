18a074368fbb7ed61eea11d5e2b60ef5c1c7bfc1
Erko Bridee <erko.bridee@gmail.com>
2015-03-11 17:51:45 -0300

    screenshot/from_file reviewed

--- javascript/screenshot/phantomjs/from_file/README.md
+++ javascript/screenshot/phantomjs/from_file/README.md
@@ -7,3 +7,21 @@ $ phantomjs image_from_htmlfile.js basic
 
 $ phantomjs image_from_htmlfile.js bootstrap_example
 ```
+
+## Links
+
+* [JavaScript Call Funtion After Page Load | MKyong.com](http://www.mkyong.com/javascript/javascript-call-funtion-after-page-load/)
+
+* [PhantomJS - API](http://phantomjs.org/api/)
+
+  * [File System Module](http://phantomjs.org/api/fs/)
+
+    * [write](http://phantomjs.org/api/fs/method/write.html)
+
+  * [Web Page Module](http://phantomjs.org/api/webpage/)
+
+    * [renderBase64](http://phantomjs.org/api/webpage/method/render-base64.html)
+
+    * [onConsoleMessage](http://phantomjs.org/api/webpage/handler/on-console-message.html)
+
+    * [onCallBack](http://phantomjs.org/api/webpage/handler/on-callback.html)
--- javascript/screenshot/phantomjs/from_file/image_from_htmlfile.js
+++ javascript/screenshot/phantomjs/from_file/image_from_htmlfile.js
@@ -7,6 +7,7 @@ var page = require('webpage').create(),
 
 var htmlfile = 'basic.html',
     imagefile = 'basic.png',
+    text64File = 'basic.base64.txt',
     dimensions = '1028x768x0',
     pageWidth = 1024,
     pageHeight = 768,
@@ -17,6 +18,7 @@ filename = system.args[1];
 if( filename ) {
   htmlfile = filename + '.html';
   imagefile = filename + '.png';
+  text64File = filename + '.base64.txt';
 }
 
 if( system.args[2] ) dimensions = system.args[2];
@@ -35,18 +37,48 @@ var curdir = fs.workingDirectory;
 
 htmlfile = curdir + fs.separator + 'html' + fs.separator + htmlfile;
 imagefile = 'screen' + fs.separator + imagefile;
+text64File = 'screen' + fs.separator + text64File;
 
 console.log( htmlfile );
 console.log( imagefile );
 
+//---
+
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+  var output = 'CONSOLE: ' + msg;
+  if( lineNum ) {
+    output += ' (from line #' + lineNum + ' in "' + sourceId + '")';
+  }
+  console.log( output );
+};
+
+page.onCallback = function( data ) {
+  console.log( 'CALLBACK: ' + JSON.stringify(data) );
+};
+
+//---
+
+function handleWithDoc( doc ) {
+  if(doc.addText) doc.addText( '-<|::[[ msg from phantomjs image_from_htmlfile.js ]]::|>-' );
+}
+
+//---
+
 page.open(htmlfile, function() {
+
+  page.evaluate(function(fn) { fn( document ); }, handleWithDoc);
 
   page.evaluate(function() {
     return document.body.bgColor = 'white';
   });
 
   setTimeout(function() {
+
     page.render( imagefile );
+
+    var base64 = page.renderBase64('PNG');
+    var imgBase64 = 'data:image/png;base64,' + base64;
+    fs.write( text64File, imgBase64, 'w' );
 
     phantom.exit();
   }, delay);
