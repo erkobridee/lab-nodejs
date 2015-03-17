98d37f7e0bd105ef67ad92cd0988f4827ea437a7
Erko Bridee <erko.bridee@gmail.com>
2015-03-17 16:32:12 -0300

    simple http web server updated

--- javascript/web/http/simpleHttpServer.js
+++ javascript/web/http/simpleHttpServer.js
@@ -1,9 +1,11 @@
-var http = require("http");
+var http = require("http"),
+    port = 9000;
 
 http.createServer(
   function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Web! \n");
   }
-).listen(9000);
-// ).listen(1337);
+).listen( port );
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
