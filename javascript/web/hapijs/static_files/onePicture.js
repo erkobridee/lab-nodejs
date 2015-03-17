72f3caabfa0b4fab0c39879a16b21469c745d95f
Erko Bridee <erko.bridee@gmail.com>
2015-03-17 17:59:29 -0300

    web/hapijs/static_files - onePicture example

---
+++ javascript/web/hapijs/static_files/onePicture.js
@@ -0,0 +1,43 @@
+var Hapi = require('hapi'),
+    PORT = 9000;
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+
+server.connection({
+  port: PORT
+});
+
+//------------------------------------------------------------------------------
+// @begin: routes
+
+server.route({
+  method: 'GET',
+  path:'/',
+  handler: function (request, reply) {
+    var indexText = '<pre>' + [
+      'hapi.js index',
+      '',
+      'try:',
+      '  /picture',
+    ].join('\n') + '</pre>';
+
+   reply(indexText);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/picture',
+  handler: function (request, reply) {
+    reply.file('./public/images/picture.png');
+  }
+});
+
+// @end: routes
+//------------------------------------------------------------------------------
+
+// Start the server
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
