1aa91d51c8a8e2b49b50624e020e7f78fb85d1e4
Erko Bridee <erko.bridee@gmail.com>
2015-04-10 18:04:04 -0300

    Karma Runner / 05 gulp / 03 - example defined

---
+++ javascript/runner/test/karma/05_gulp/03/tests/require.config.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+require.config({
+
+  // Karma serves files under /base, which is the basePath from your config file
+  baseUrl: '/base/src',
+
+  // libraries dependencies (fallback support)
+  paths: {
+
+    jquery: [
+      'bower_components/jquery/dist/jquery.min'
+    ]
+
+  },
+
+  // define js scripts dependencies
+  shim: {
+
+    'jquery': {
+      exports: 'jquery'
+    }
+
+  },
+
+  priority: [
+    'jquery'
+  ],
+
+  deps: [
+    './app/run',
+    '../tests/require.specs.load'
+  ],
+
+  callback: onRequireReadyHandler
+
+});
+
+
+function onRequireReadyHandler() {
+  console.log( 'onRequireReadyHandler' );
+
+  window.__karma__.start();
+}
+
+})();
+
