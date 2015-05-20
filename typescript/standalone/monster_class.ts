5e51c987fc44976417cfde7148f2be14162fb502
Erko Bridee <erko.bridee@gmail.com>
2015-05-20 16:57:45 -0300

    typescript - class test

---
+++ typescript/standalone/monster_class.ts
@@ -0,0 +1,27 @@
+interface Point {
+	x: Number,
+	y: Number
+};
+
+class Monster1 {
+	name: String;
+	position: Point;
+	
+	constructor(name, position) {
+		this.name = name;
+		this.position = position;
+	}
+}
+
+class Monster2 {
+	constructor(public name:String, public position: Point) {
+		
+	}
+}
+
+//---
+
+var monster1 = new Monster1('Monster Name 01', {x:1, y:2});
+var monster2 = new Monster2('Monster Name 02', {x:2, y:6});
+
+console.log( monster1, monster2 );
