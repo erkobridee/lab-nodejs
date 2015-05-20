a991d3ceb544aaf43b8d82f142a8c9da0774a5c6
Erko Bridee <erko.bridee@gmail.com>
2015-05-20 16:47:02 -0300

    typescript - interface test

---
+++ typescript/standalone/contact_interface.ts
@@ -0,0 +1,29 @@
+interface Contact {
+	name: String,
+	email: String,
+	twitter: String
+}
+
+var contacts: Contact[] = [];
+
+var contact: Contact;
+
+contact = {
+	name: 'Erko Bridee',
+	email: 'my@mail.ext',
+	twitter: 'erkobridee'
+};
+
+contacts.push( contact );
+
+contact = {
+	name: 'Other Person',
+	email: 'person@other.ext',
+	twitter: 'otherperson'
+};
+
+contacts.push( contact );
+
+//---
+
+console.log( contacts );
