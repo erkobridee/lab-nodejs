a2ff317488fc1bfdfa4ba68f7e4f2f1622bccd89
Erko Bridee <erko.bridee@gmail.com>
2015-05-22 15:42:44 -0300

    typescript code format updated

--- typescript/standalone/monster_class.ts
+++ typescript/standalone/monster_class.ts
@@ -6,21 +6,21 @@ interface Point {
 class Monster1 {
   name: String;
   position: Point;
-	
-	constructor( name, position ) {
+
+  constructor(name, position) {
     this.name = name;
     this.position = position;
-	}
+  }
 }
 
 class Monster2 {
-  constructor( public name:String, public position: Point ) {
+  constructor(public name: String, public position: Point) {
   }
 }
 
 //---
 
-var monster1 = new Monster1('Monster Name 01', {x:1, y:2});
-var monster2 = new Monster2('Monster Name 02', {x:2, y:6});
+var monster1 = new Monster1('Monster Name 01', { x: 1, y: 2 });
+var monster2 = new Monster2('Monster Name 02', { x: 2, y: 6 });
 
-console.log( monster1, monster2 );
+console.log(monster1, monster2);
