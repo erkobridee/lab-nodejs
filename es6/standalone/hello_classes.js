73bb6a5be04dd1ca09743536e99cdc14b747928e
Erko Bridee <erko.bridee@gmail.com>
2015-05-22 14:30:25 -0300

    es6 - hello classes in es6

---
+++ es6/standalone/hello_classes.js
@@ -0,0 +1,39 @@
+/*
+  Classes in ECMAScript 6 (final semantics) | 2ality
+  http://www.2ality.com/2015/02/es6-classes-final.html
+*/
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  toString() {
+    return '( ' + this.x + ', ' + this.y + ' )';
+  }
+}
+
+let point = new Point( 1, 1 );
+
+console.log( 'point : ', point );
+console.log( 'point instanceof Point : ', point instanceof Point );
+
+//---
+
+class ColorPoint extends Point {
+  constructor(x, y, color) {
+    super(x, y);
+    this.color = color;
+  }
+  
+  toString() {
+    return super.toString() + ' in ' + this.color;
+  }
+}
+
+let colorPoint = new ColorPoint(2, 2, 'blue');
+
+console.log( 'colorPoint : ', colorPoint );
+console.log( 'colorPoint instanceof ColorPoint: ', colorPoint instanceof ColorPoint );
+console.log( 'colorPoint instanceof Point: ', colorPoint instanceof Point );
