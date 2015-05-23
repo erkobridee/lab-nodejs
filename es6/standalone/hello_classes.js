e76075f35c169993246416330e1b4af77e14cd11
Erko Bridee <erko.bridee@gmail.com>
2015-05-22 22:22:48 -0300

    es6/standalone/hello_classes.js - updated

--- es6/standalone/hello_classes.js
+++ es6/standalone/hello_classes.js
@@ -8,15 +8,15 @@ class Point {
     this.x = x;
     this.y = y;
   }
-  
+
   toString() {
-    return '( ' + this.x + ', ' + this.y + ' )';
+    return `( ${this.x}, ${this.y} )`;
   }
 }
 
 let point = new Point( 1, 1 );
 
-console.log( 'point : ', point );
+console.log( 'point : ', point.toString() );
 console.log( 'point instanceof Point : ', point instanceof Point );
 
 //---
@@ -26,14 +26,14 @@ class ColorPoint extends Point {
     super(x, y);
     this.color = color;
   }
-  
+
   toString() {
-    return super.toString() + ' in ' + this.color;
+    return `${super.toString()} in ${this.color}`;
   }
 }
 
 let colorPoint = new ColorPoint(2, 2, 'blue');
 
-console.log( 'colorPoint : ', colorPoint );
+console.log( 'colorPoint : ', colorPoint.toString() );
 console.log( 'colorPoint instanceof ColorPoint: ', colorPoint instanceof ColorPoint );
 console.log( 'colorPoint instanceof Point: ', colorPoint instanceof Point );
