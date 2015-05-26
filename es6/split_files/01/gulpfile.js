c8ace4b317c485db1536f3f764f9499512993a36
Erko Bridee <erko.bridee@gmail.com>
2015-05-26 02:19:05 -0300

    es6 / split files / 01 - updated
    gulp-babel dep removed and code reviewed

--- es6/split_files/01/gulpfile.js
+++ es6/split_files/01/gulpfile.js
@@ -1,13 +1,12 @@
 var del        = require('del');
 var gulp       = require('gulp');
-var babel      = require('gulp-babel');
 var babelify   = require('babelify');
 var browserify = require('browserify');
 var source     = require('vinyl-source-stream');
 
 gulp.task('clean', del.bind(null, [ 'dist' ]));
 
-gulp.task('compile', function() {
+gulp.task('compile', ['clean'], function() {
   browserify({
     entries: './src/app.js',
     debug: true
