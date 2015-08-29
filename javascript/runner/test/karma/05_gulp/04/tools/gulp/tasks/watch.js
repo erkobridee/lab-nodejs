6085e0c622bd65b75451878ca3219bffc02869b9
Erko Bridee <erko.bridee@gmail.com>
2015-08-29 00:48:19 -0300

    feat(tools): define initial watch gulp tasks

--- javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/watch.js
+++ javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/watch.js
@@ -1,5 +1,27 @@
 module.exports = function(gulp, $) {
 
-  // TODO: define gulp tasks
+  // TODO: review
+
+  // wf - watch flow
+
+    // bs - browser sync
+  gulp.task('wf:bs:reload', function() {
+    if( $.browserSync.active ) $.reload();
+  });
+
+  gulp.task('wf', function( done ) {
+    $.runSequence(
+      'jshint',
+      [
+        'wf:bs:reload',
+        'karma:unit:run'
+      ],
+      done
+    );
+  });
+
+  gulp.task('watch', function() {
+    gulp.watch(['{src,tests}/**/*.js'], ['wf']);
+  });
 
 };
