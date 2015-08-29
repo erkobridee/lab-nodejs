8282d229eaf775b2b3bf59793a1f85f5a46233af
Erko Bridee <erko.bridee@gmail.com>
2015-08-29 00:45:11 -0300

    feat(tools): define initial karma gulp tasks

--- javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/karma.js
+++ javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/karma.js
@@ -1,5 +1,35 @@
 module.exports = function(gulp, $) {
 
-  // TODO: define gulp tasks
+  // TODO: review
+
+  gulp.task('karma:unit:single-run', function( done ) {
+    $.karma.server.start( $.config.karma.unitSingleRun, done );
+  });
+
+  gulp.task('karma:unit', ['karma:unit:single-run'], function() {
+    $.karma.background( $.config.karma.unit );
+    $.karma.background.running = true;
+  });
+
+  gulp.task('karma:unit:run', function( done ) {
+    if( $.karma.background.running ) {
+      $.karma.runner.run( $.config.karma.unit, done );
+    } else {
+      done();
+    }
+  });
+
+  gulp.task('karma:unit:dev', function( done ) {
+    $.karma.server.start( $.config.karma.unit, done );
+  });
+
+
+  gulp.task('karma:coverage', function( done ) {
+    $.karma.server.start( $.config.karma.coverage, done );
+  });
+
+  gulp.task('karma:ci', function( done ) {
+    $.karma.server.start( $.config.karma.ci, done );
+  });
 
 };
