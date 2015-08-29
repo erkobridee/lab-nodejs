fadd1a1abec0a40b637e0041ff62f568a4709c88
Erko Bridee <erko.bridee@gmail.com>
2015-08-29 01:01:49 -0300

    feat(tools): add more default gulp tasks

--- javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/default.js
+++ javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/default.js
@@ -1,9 +1,55 @@
 module.exports = function(gulp, $) {
 
-  gulp.task('default', function() {
+  gulp.task('default', ['jshint', 'clean'], function() {
     $.projectInfoMsg();
   });
 
-  // TODO: define gulp tasks
+  // TODO: review
+
+  gulp.task('project', function( done ) {
+    $.runSequence(
+      'default',
+      'webserver',
+      'watch',
+      done
+    );
+  });
+
+  gulp.task('coverage', function( done ) {
+    $.runSequence(
+      'default',
+      'karma:coverage',
+      done
+    );
+  });
+
+  gulp.task('ci', function( done ) {
+    $.runSequence(
+      'default',
+      'karma:ci',
+      done
+    );
+  });
+
+  gulp.task('specs', function( done ) {
+    $.runSequence(
+      'default',
+      'karma:unit',
+      'watch',
+      done
+    );
+  });
+
+  gulp.task('dev', function( done ) {
+    $.runSequence(
+      'default',
+      [
+        'webserver',
+        'karma:unit'
+      ],
+      'watch',
+      done
+    );
+  });
 
 };
