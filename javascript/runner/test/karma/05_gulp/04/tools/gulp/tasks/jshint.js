21c2244d5807828ef89a3fd5182846035cb9a85d
Erko Bridee <erko.bridee@gmail.com>
2015-08-29 00:42:17 -0300

    feat(tools): define initial jshint gulp tasks config

--- javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/jshint.js
+++ javascript/runner/test/karma/05_gulp/04/tools/gulp/tasks/jshint.js
@@ -1,5 +1,28 @@
 module.exports = function(gulp, $) {
 
-  // TODO: define gulp tasks
+  // TODO: review
+
+  var jshintString = $.lazypipe()
+    .pipe( $.cached, 'jshint' )
+    .pipe( $.jshint )
+    .pipe( $.jshint.reporter, 'jshint-stylish' )
+    .pipe( $.jshint.reporter, 'fail' );
+
+  gulp.task('jshint:project', function() {
+    return gulp.src( $.config.jshint.project )
+      .pipe( jshintString() );
+  });
+
+  // gulp.task('jshint:specs', function() {
+  //   return gulp.src( $.config.jshint.specs )
+  //     .pipe( jshintString() );
+  // });
+
+  gulp.task('jshint:tools', function() {
+    return gulp.src( $.config.jshint.tools )
+      .pipe( jshintString() );
+  });
+
+  gulp.task('jshint', ['jshint:project', 'jshint:specs', 'jshint:tools']);
 
 };
