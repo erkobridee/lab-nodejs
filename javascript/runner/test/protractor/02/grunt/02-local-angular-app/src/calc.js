897a49c80dd8189efd3d878354a8b552d842bb03
Erko Bridee <erko.bridee@gmail.com>
2015-10-12 22:16:22 -0300

    refactor(protractor): update basic grunt local example
    - bower dependencies
    - node/grunt dependencies

--- javascript/runner/test/protractor/02/grunt/02-local-angular-app/src/calc.js
+++ javascript/runner/test/protractor/02/grunt/02-local-angular-app/src/calc.js
@@ -1,56 +1,67 @@
-var CalcCtrl = function($timeout, $scope) {
-  $scope.memory = [];
-  $scope.latest = 0;
-  $scope.operators = {
-    ADDITION: '+',
-    SUBTRACTION: '-',
-    MULTIPLICATION: '*',
-    DIVISION: '/',
-    MODULO: '%'
-  };
-  $scope.operator = $scope.operators.ADDITION;
+(function(){
+  'use strict';
 
-  $scope.doAddition = function() {
-    var times = 5;
-    $scope.latest = '. ';
-    $timeout(function tickslowly() {
-      if (times === 0) {
-        var latestResult;
-        var first = parseInt($scope.first);
-        var second = parseInt($scope.second);
-        switch ($scope.operator) {
-          case '+':
-            latestResult = first + second;
-            break;
-          case '-':
-            latestResult = first - second;
-            break;
-          case '*':
-            latestResult = first * second;
-            break;
-          case '/':
-            latestResult = first / second;
-            break;
-          case '%':
-            latestResult = first % second;
-            break;
+  function CalcCtrl($timeout, $scope) {
+    $scope.memory = [];
+    $scope.latest = 0;
+    $scope.operators = {
+      ADDITION: '+',
+      SUBTRACTION: '-',
+      MULTIPLICATION: '*',
+      DIVISION: '/',
+      MODULO: '%'
+    };
+    $scope.operator = $scope.operators.ADDITION;
+
+    $scope.doAddition = function() {
+      var times = 5;
+      $scope.latest = '. ';
+      $timeout(function tickslowly() {
+        if (times === 0) {
+          var latestResult;
+          var first = parseInt($scope.first);
+          var second = parseInt($scope.second);
+          switch ($scope.operator) {
+            case '+':
+              latestResult = first + second;
+              break;
+            case '-':
+              latestResult = first - second;
+              break;
+            case '*':
+              latestResult = first * second;
+              break;
+            case '/':
+              latestResult = first / second;
+              break;
+            case '%':
+              latestResult = first % second;
+              break;
+          }
+          $scope.memory.unshift({
+            timestamp: new Date(),
+            first: $scope.first,
+            operator: $scope.operator,
+            second: $scope.second,
+            value: latestResult
+          });
+          $scope.first = $scope.second = '';
+          $scope.latest = latestResult;
+        } else {
+          $scope.latest += '. ';
+          times--;
+          $timeout(tickslowly, 300);
         }
-        $scope.memory.unshift({
-          timestamp: new Date(),
-          first: $scope.first,
-          operator: $scope.operator,
-          second: $scope.second,
-          value: latestResult
-        });
-        $scope.first = $scope.second = '';
-        $scope.latest = latestResult;
-      } else {
-        $scope.latest += '. ';
-        times--;
-        $timeout(tickslowly, 300);
-      }
-    }, 300);
-  };
-};
+      }, 300);
+    };
+  }
 
-var calculator = angular.module('calculator', []);
+  //---
+
+  CalcCtrl.$inject = ['$timeout', '$scope'];
+
+  angular
+    .module('calculator', [])
+    .controller('CalcCtrl', CalcCtrl);
+
+})();
