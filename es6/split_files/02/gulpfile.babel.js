8856754b0214e5d71c1cce9e95ab2860c6b94cb8
Erko Bridee <erko.bridee@gmail.com>
2016-05-27 22:23:26 -0300

    es6/split_files/02 - use gulpfile with es6 sintax

---
+++ es6/split_files/02/gulpfile.babel.js
@@ -0,0 +1,39 @@
+import del from 'del';
+import gulp from 'gulp';
+import shell from 'gulp-shell';
+import eslint from 'gulp-eslint';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+
+gulp.task('clean', del.bind(null, [ 'dist' ]));
+
+gulp.task('eslint', () => {
+  return gulp
+    .src(['./src/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('compile', ['clean', 'eslint'], () => {
+  return browserify({
+    entries: './src/main.js',
+    debug: true
+  })
+  .transform(babelify)
+  .bundle()
+  .pipe(source('main.js'))
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('run:app', ['compile'], shell.task([
+  'node dist/main.js'
+]));
+
+gulp.task('watch', ['run:app'], () => {
+
+  gulp.watch(['./src/**/*.js'], ['run:app']);
+
+});
+
+gulp.task('default', ['watch']);
