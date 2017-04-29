e4925e0057ef26e761e5eaba4bfb824ed45bace9
Erko Bridee <erko.bridee@gmail.com>
2017-04-29 18:42:03 -0300

    refactor gulpfile.js on js es5 browserify gulp project

--- js_until_es5.1/bundlers/browserify/gulp/gulpfile.js
+++ js_until_es5.1/bundlers/browserify/gulp/gulpfile.js
@@ -1,13 +1,22 @@
 var del        = require('del');
 var gulp       = require('gulp');
+var gutil      = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify     = require('gulp-uglify');
 var jshint     = require('gulp-jshint');
 var shell      = require('gulp-shell');
 var browserify = require('browserify');
 var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
 
 //------------------------------------------------------------------------------
 
 var config = {};
+
+config.outputFilename = {
+  dev : 'main.js',
+  prod : 'main.min.js'
+};
 
 config.paths = {
   src: './src',
@@ -17,7 +26,8 @@ config.paths = {
 config.js = {
   files: config.paths.src + '/**/*.js',
   main: config.paths.src + '/main.js',
-  app: config.paths.dist + '/main.js',
+  devApp: config.paths.dist + '/' + config.outputFilename.dev,
+  prodApp: config.paths.dist + '/' + config.outputFilename.prod
 };
 
 //------------------------------------------------------------------------------
@@ -32,23 +42,49 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('compile', ['clean', 'jshint'], function() {
+//---
+
+gulp.task('compile:dev', ['clean', 'jshint'], function() {
   return browserify({
     entries: config.js.main,
-    debug: true
+    debug: true // will generate sourcemaps output inside of bundle file
   })
   .bundle()
-  .pipe(source('main.js'))
+  .pipe(source(config.outputFilename.dev))
   .pipe(gulp.dest( config.paths.dist ));
 });
 
-gulp.task('run:app', ['compile'], shell.task([
-  'node ' + config.js.app
+gulp.task('compile:prod', ['clean', 'jshint'], function() {
+  return browserify({
+    entries: config.js.main
+  })
+  .bundle()
+  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+  .pipe(source(config.outputFilename.prod))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+  .pipe(uglify())
+  .pipe(sourcemaps.write('./')) // writes .map file
+  .pipe(gulp.dest( config.paths.dist ));
+});
+
+gulp.task('compile', ['compile:dev']);
+
+//---
+
+gulp.task('run:app:dev', ['compile:dev'], shell.task([
+  'node ' + config.js.devApp
 ]));
 
-gulp.task('watch', ['run:app'], function() {
+gulp.task('run:app:prod', ['compile:prod'], shell.task([
+  'node ' + config.js.prodApp
+]));
 
-  gulp.watch([ config.js.files ], ['run:app']);
+//---
+
+gulp.task('watch', ['run:app:dev'], function() {
+
+  gulp.watch([ config.js.files ], ['run:app:dev']);
 
 });
 
