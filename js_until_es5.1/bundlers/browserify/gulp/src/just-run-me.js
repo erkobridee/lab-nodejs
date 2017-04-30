10b97c2264c7fc0ce11c9d83cb567a53c42ce76b
Erko Bridee <erko.bridee@gmail.com>
2017-04-30 00:04:22 -0300

    update js code on js es5 browserify gulp project

---
+++ js_until_es5.1/bundlers/browserify/gulp/src/just-run-me.js
@@ -0,0 +1,5 @@
+console.log('');
+console.log('------------------------------------------');
+console.log('just running \'just-run-me.js\' content');
+console.log('------------------------------------------');
+console.log('');
--- js_until_es5.1/bundlers/browserify/gulp/src/main.js
+++ js_until_es5.1/bundlers/browserify/gulp/src/main.js
@@ -1,3 +1,6 @@
 var sum = require('./lib/sum');
 
-console.log(sum(1, 1));
+console.log('');
+console.log('sum of 1 + 1 = ', sum(1, 1));
+
+require('./just-run-me');
