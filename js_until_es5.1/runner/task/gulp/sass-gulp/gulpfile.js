1c66f0539ca7574b67b5642650424cd30eac8f9e
Erko Bridee <erko.bridee@gmail.com>
2017-04-29 21:33:12 -0300

    refactor gulpfile.js on js es5 gulp sass-gulp project

--- js_until_es5.1/runner/task/gulp/sass-gulp/gulpfile.js
+++ js_until_es5.1/runner/task/gulp/sass-gulp/gulpfile.js
@@ -1,23 +1,64 @@
-var del         = require('del');
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var sourcemaps  = require('gulp-sourcemaps');
+var del           = require('del');
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var sourcemaps    = require('gulp-sourcemaps');
+var autoprefixer  = require('gulp-autoprefixer');
 
-var config = {
+//------------------------------------------------------------------------------
+
+var config = {};
+
+config.paths = {
   src: './src',
   dist: './dist'
 };
 
-gulp.task('clean:css', del.bind(null, [ config.dist ]));
+config.sass = {
+  files : config.paths.src + '/**/*.scss'
+};
 
-gulp.task('clean', ['clean:css']);
+//------------------------------------------------------------------------------
 
-gulp.task('build:css', function() {
-  return gulp.src(config.src+'/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.dist));
+gulp.task('clean', del.bind(null, [ config.paths.dist ]));
+
+//---
+
+gulp.task('sass:dev', ['clean:css'], function() {
+  return gulp.src(config.sass.files)
+    .pipe(sass({
+      errLogToConsole : true,
+      outputStyle : 'expanded'
+    }))
+    .on('error', sass.logError)
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(config.paths.dist));
 });
 
-gulp.task('default', ['clean:css', 'build:css']);
+gulp.task('sass:prod', ['clean:css'], function() {
+  return gulp.src(config.sass.files)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole : true,
+      outputStyle : 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('build:dev', ['sass:dev']);
+gulp.task('build:prod', ['sass:prod']);
+
+//---
+
+gulp.task('watch', ['clean', 'build:dev'], function() {
+
+  gulp.watch([ config.sass.files ], ['sass:dev']);
+
+});
+
+//---
+
+// development
+gulp.task('default', ['watch']);
