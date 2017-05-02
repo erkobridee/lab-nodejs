7f29ba374975d0837e7716a990896467a895c867
Erko Bridee <erko.bridee@gmail.com>
2017-05-02 18:27:17 -0300

    add sass-bootstrap3-grunt project on js es5 grunt

---
+++ js_until_es5.1/runner/task/grunt/sass-bootstrap3-grunt/Gruntfile.js
@@ -0,0 +1,241 @@
+module.exports = function(grunt){
+  'use strict';
+
+  require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
+
+  //---
+
+  var projectConfig = {};
+
+  projectConfig.paths = {
+    bower : 'bower_components',
+    src : 'src',
+    build : '.temp',
+    dist : 'dist'
+  };
+
+  projectConfig.htmlIndexBuild = projectConfig.paths.build + '/index.html';
+  projectConfig.htmlAssetsFiles = {};
+  projectConfig.htmlAssetsFiles[projectConfig.htmlIndexBuild] = [
+    projectConfig.paths.build + '/**/*.js',
+    projectConfig.paths.build + '/**/*.css'
+  ];
+
+  var bootstrapPath = projectConfig.paths.bower + '/bootstrap-sass/assets';
+  var jqueryPath = projectConfig.paths.bower + '/jquery/dist';
+  projectConfig.vendor = {
+    compassMixins : {
+      path : projectConfig.paths.bower + '/compass-mixins/lib'
+    },
+    bootstrap : {
+      path : bootstrapPath,
+      styles : bootstrapPath + '/stylesheets',
+      fonts : bootstrapPath + '/fonts',
+      javascript : bootstrapPath + '/javascripts/bootstrap.js'
+    },
+    jquery : {
+      path : jqueryPath,
+      javascript : jqueryPath + '/jquery.js'
+    }
+  };
+  bootstrapPath = null;
+  jqueryPath = null;
+
+  // project styles
+  var sassBuildFiles = {};
+  var sassBuildedFile = projectConfig.paths.build + '/css/styles.css';
+  sassBuildFiles[sassBuildedFile] = (projectConfig.paths.src + '/styles/main.scss');
+  projectConfig.sass = {
+    buildFiles : sassBuildFiles,
+    builded : sassBuildedFile,
+    includePaths : [
+      projectConfig.vendor.compassMixins.path,
+      projectConfig.vendor.bootstrap.styles
+    ]
+  };
+  sassBuildedFile = null;
+  sassBuildFiles = null;
+
+  projectConfig.output = {
+    javascripts : projectConfig.paths.build + '/js',
+    fonts : projectConfig.paths.dist + '/fonts'
+  };
+
+  //---
+
+  var gruntConfig = {
+    pkg : grunt.file.readJSON('package.json'),
+
+    config : projectConfig,
+
+    jshint : {
+      tools : 'Gruntfile.js',
+      project : '<%= config.paths.src %>/**/*.js'
+    },
+
+    clean : {
+      build : ['<%= config.paths.build %>'],
+      jsbuild : ['<%= config.paths.build %>/js'],
+      jsminbuild : ['<%= config.paths.build %>/js-min'],
+      dist : ['<%= config.paths.dist %>']
+    },
+
+    copy : {
+      bootstrapFonts : {
+        expand : true,
+        cwd : '<%= config.vendor.bootstrap.fonts %>',
+        src : '**',
+        dest : '<%= config.output.fonts %>'
+      },
+
+      jsmin : {
+        expand : true,
+        cwd : '<%= config.paths.build %>/js-min',
+        src : '**',
+        dest : '<%= config.paths.build %>/js'
+      },
+
+      indexhtml2build : {
+        expand : true,
+        cwd : '<%= config.paths.src %>',
+        src : 'index.html',
+        dest : '<%= config.paths.build %>'
+      },
+
+      build2dist : {
+        expand : true,
+        cwd : '<%= config.paths.build %>',
+        src : '**',
+        dest : '<%= config.paths.dist %>'
+      }
+    }, //@end: copy
+
+    sass : {
+      options : {
+        errLogToConsole : true,
+        includePaths : '<%= config.sass.includePaths %>'
+      },
+      dev : {
+        options : {
+          outputStyle : 'expanded'
+        },
+        files : '<%= config.sass.buildFiles %>'
+      },
+      prod : {
+        options : {
+          outputStyle : 'compressed',
+          sourceMap : true
+        },
+        files : '<%= config.sass.buildFiles %>'
+      }
+    }, // @end: sass
+
+    postcss : {
+      options : {
+        processors : [
+          require('autoprefixer')
+        ]
+      },
+      dev : {
+        src : '<%= config.sass.builded %>'
+      },
+      prod : {
+        options : {
+          map : true
+        },
+        src : '<%= config.sass.builded %>'
+      }
+    }, // @end: postcss
+
+    concat : {
+      vendor : {
+        src : [
+          '<%= config.vendor.bootstrap.javascript %>',
+          '<%= config.vendor.jquery.javascript %>'
+        ],
+        dest : '<%= config.output.javascripts %>/vendor.js'
+      },
+
+      project : {
+        src : [
+          '<%= config.paths.src %>/**/*.js'
+        ],
+        dest : '<%= config.output.javascripts %>/app.js'
+      }
+    }, // @end: concat
+
+    filerev : {
+      css : {
+        src : '<%= config.paths.build %>/css/**/*.css'
+      },
+      js : {
+        src : '<%= config.paths.build %>/js/**/*.js'
+      }
+    }, // @end: filerev
+
+    uglify : {
+      options : {
+        sourceMap : true
+      },
+      prod : {
+        files : [{
+          expand : true,
+          cwd : '<%= config.paths.build %>/js/',
+          src : ['*.js', '!*.min.js'],
+          dest : '<%= config.paths.build %>/js-min/',
+          ext : '.min.js'
+        }]
+      }
+    }, // @end: uglify
+
+    injector : {
+      options : {
+        addRootSlash : false,
+        ignorePath : '<%= config.paths.build %>'
+      },
+      assets : {
+        files : '<%= config.htmlAssetsFiles %>'
+      }
+    }
+  };
+  grunt.initConfig(gruntConfig);
+
+  //---
+
+  grunt.registerTask('common:tasks', [
+    'clean',
+    'copy:bootstrapFonts',
+    'copy:indexhtml2build'
+  ]);
+
+  grunt.registerTask('build:dev', [
+     'common:tasks',
+     'sass:dev',
+     'postcss:dev',
+     'concat:vendor',
+     'concat:project',
+     'injector',
+     'copy:build2dist',
+     'clean:build'
+  ]);
+
+  grunt.registerTask('build:prod', [
+    'common:tasks',
+    'sass:prod',
+    'postcss:prod',
+    'concat:vendor',
+    'concat:project',
+    'uglify',
+    'clean:jsbuild',
+    'copy:jsmin',
+    'clean:jsminbuild',
+    'filerev',
+    'injector',
+    'copy:build2dist',
+    'clean:build'
+  ]);
+
+  grunt.registerTask('default', ['jshint', 'build:prod']);
+
+};
---
+++ js_until_es5.1/runner/task/grunt/sass-bootstrap3-grunt/README.md
@@ -0,0 +1,141 @@
+# Sass Bootstrap 3 Gulp
+
+> compile [sass](http://sass-lang.com/) files and use bootstrapp sass official sass files imported in the project sass
+
+* [[GitHub] twbs / bootstrap-sass](https://github.com/twbs/bootstrap-sass) - Official Sass port of Bootstrap 2 and 3
+
+  * make sure, before import sass files into your sass, have the line `$bootstrap-sass-asset-helper: false !default;`
+
+  * in case you have used any [compass](http://compass-style.org/) mixin ([compass mixins list](http://compass-style.org/index/mixins/)), the [compass-mixins](https://github.com/Igosuki/compass-mixins) may help to compile your sass
+
+## Prerequisites
+
+* Must have [Git](http://git-scm.com/) installed
+
+* Must have [node.js (at least LTS)](http://nodejs.org/) installed with npm (Node Package Manager)
+
+* Must have [Grunt](https://github.com/gruntjs/grunt) node package installed globally.  `sudo npm install -g grunt-cli`
+
+
+## Commands on Mac
+
+### Create commands list
+
+```
+mkdir sass-bootstrap3-grunt
+
+cd sass-bootstrap3-grunt
+
+touch README.md
+
+touch Gruntfile.js
+
+// create src/ files
+
+bower init
+
+bower install \
+  jquery \
+  bootstrap-sass-official \
+  compass-mixins \
+  --save
+
+npm init
+
+npm install \
+  grunt \
+  grunt-contrib-jshint \
+  grunt-contrib-clean \
+  grunt-contrib-copy \
+  grunt-contrib-concat \
+  grunt-contrib-uglify \
+  grunt-sass \
+  grunt-postcss \
+  autoprefixer \
+  grunt-injector \
+  grunt-filerev \
+  time-grunt \
+  jit-grunt \
+  --save-dev
+
+```
+
+### project cloned from git
+
+```
+npm install
+```
+
+### run command
+
+* cleanup `dist` folder
+
+```
+grunt clean
+```
+
+* build development
+
+```
+grunt build:dev
+```
+
+* build production
+
+```
+grunt build:prod
+```
+
+* production build flow
+
+```
+grunt
+```
+
+## Links
+
+* [Sass: Syntactically Awesome Style Sheets](http://sass-lang.com/) - [guide](http://sass-lang.com/guide)
+
+* [[Udemy] Sass: From Beginner to Advanced](https://www.udemy.com/sass-from-beginner-to-advanced/)
+
+* [compass](http://compass-style.org/)
+
+### dependencies
+
+#### bower
+
+* [[GitHub] jquery / jquery](https://github.com/jquery/jquery) - jQuery JavaScript Library
+
+* [[GitHub] twbs / bootstrap-sass](https://github.com/twbs/bootstrap-sass) - Official Sass port of Bootstrap 2 and 3.
+
+* [[GitHub] Igosuki / compass-mixins](https://github.com/Igosuki/compass-mixins) - A collection of compass' stylesheet for bower dependencies and libsass
+
+### dev dependencies
+
+#### node.js
+
+* [[GitHub] gruntjs / grunt](https://github.com/gruntjs/grunt) - Grunt: The JavaScript Task Runner
+
+* [[GitHub] gruntjs / grunt-contrib-jshint](https://github.com/gruntjs/grunt-contrib-jshint) - Validate files with JSHint
+
+* [[GitHub] gruntjs / grunt-contrib-copy](https://github.com/gruntjs/grunt-contrib-copy) - Copy files and folders.
+
+* [[GitHub] gruntjs / grunt-contrib-clean](https://github.com/gruntjs/grunt-contrib-clean) - Clear files and folders.
+
+* [[GitHub] gruntjs / grunt-contrib-concat](https://github.com/gruntjs/grunt-contrib-concat) - Concatenate files.
+
+* [[GitHub] gruntjs / grunt-contrib-uglify](https://github.com/gruntjs/grunt-contrib-uglify) - Minify files with UglifyJS.
+
+* [[GitHub] sindresorhus / grunt-sass](https://github.com/sindresorhus/grunt-sass) - Compile Sass to CSS
+
+* [[GitHub] nDmitry / grunt-postcss](https://github.com/nDmitry/grunt-postcss) - Apply several post-processors to your CSS using PostCSS.
+
+  * [[GitHub] postcss / autoprefixer](https://github.com/postcss/autoprefixer) - Parse CSS and add vendor prefixes to rules by Can I Use
+
+* [[GitHub] klei / grunt-injector](https://github.com/klei/grunt-injector) - Inject references to files into other files (think scripts and stylesheets into an html file)
+
+* [[GitHub] yeoman / grunt-filerev](https://github.com/yeoman/grunt-filerev)
+
+* [[GitHub] sindresorhus / time-grunt](https://github.com/sindresorhus/time-grunt) - Display the elapsed execution time of grunt tasks
+
+* [[GitHub] shootaroo / jit-grunt](https://github.com/shootaroo/jit-grunt) - JIT(Just In Time) plugin loader for Grunt
