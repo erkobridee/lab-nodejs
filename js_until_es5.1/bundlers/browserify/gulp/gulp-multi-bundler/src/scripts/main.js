e1fe6ecfa5965aeedf2d77dbeb85933f7638944b
Erko Bridee <erko.bridee@gmail.com>
2017-06-02 21:38:06 -0300

    refactor(browserify es5): redefine main.js code

--- js_until_es5.1/bundlers/browserify/gulp/gulp-multi-bundler/src/scripts/main.js
+++ js_until_es5.1/bundlers/browserify/gulp/gulp-multi-bundler/src/scripts/main.js
@@ -3,6 +3,18 @@ var _ = require('lodash');
 
 var math = require('./lib/math');
 
-console.log('');
-console.log('sum of 1 + 1 = ', math.sum(1, 1));
-console.log('multiply of 2 + 3 = ', math.multiply(2, 3));
+//----------------------------------------------------------------------------//
+
+var outputs = [
+  'sum of 1 + 2 = ' + math.sum(1, 2),
+  'multiply of 2 + 3 = ' + math.multiply(2, 3)
+];
+
+var displayAreaUI = $('#displayArea');
+
+console.log('---');
+_.forEach(outputs, function(message){
+  console.log(message);
+  displayAreaUI.append('<p>'+ message +'</p>');
+});
+console.log('---');
